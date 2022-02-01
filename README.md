d342dcfe1ced2b0e693b03be251143c4d9513c10
reginmaru <reginmaru@gmail.com>
2022-02-01 19:42:39 +0000

    Added first test - Still need to work on the src

---
+++ README.md
@@ -0,0 +1,5 @@
+### Instructions
+
+# The way I thought about the task
+
+Using junit I construct unit tests to check if I can add items to my receipt/basket. 
---
+++ src/main/java/Receipt.java
@@ -0,0 +1,12 @@
+public class Receipt {
+    itemsInShop
+    public String addItem(String s){
+        if (s == "apple"){
+
+        }
+
+    }
+    public static void main(String[]args){
+
+    }
+}
